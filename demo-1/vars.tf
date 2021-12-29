2b11eb9655317ef8b9484442451651b6d3e1f907
Ryan Fong <ryanfong@rmac.local>
2021-12-29 16:20:26 +0800

    test commit

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -5,12 +5,13 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "ap-southeast-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
+    ap-southeast-1 = "ami-076ccd3e4bdbbe88a"
     us-east-1 = "ami-13be557e"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-0d729a60"
